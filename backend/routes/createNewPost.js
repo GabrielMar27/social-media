e48a0bbb1a192d9b371056e686b8f5b3cd46bb8f
Gabi <marin.gabriel.ionut27@gmail.com>
2024-03-31 13:49:11 +0200

    da

--- backend/routes/createNewPost.js
+++ backend/routes/createNewPost.js
@@ -5,20 +5,54 @@ const con = require("../database");
 router.post("/", (req, res) => {
   const { media_postare, titlu_postare, text_postare, id_user } = req.body;
   const timeStamp = new Date().toISOString().slice(0, 19).replace("T", " ");
-  con.query(
-    "INSERT INTO postari SET ?",
-    [media_postare, titlu_postare, text_postare, id_user, timeStamp],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({
-          error: `S-a pordus urmatoarea erroare:${err}`,
-        });
-      }
-      res.status(200).json({
-        OK: `Postare trimisa cu succes:${result}`,
+  const postData = {
+    titlu_postare,
+    text_postare,
+    id_user,
+    timeStamp: timeStamp,
+  };
+  con.query("INSERT INTO postari SET ?", postData, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        error: `S-a produs următoarea eroare: ${err}`,
       });
+    } else {
+      const id_postare = result.insertId;
+      const poze = media_postare.map((poza) => {
+        return new Promise((resolve, reject) => {
+          const photoData = {
+            id_postare,
+            poza: poza,
+          };
+          con.query(
+            "INSERT INTO poza_postare SET ?",
+            photoData,
+            (err, result) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(result);
+              }
+            }
+          );
+        });
+      });
+
+      Promise.all(poze)
+        .then((results) => {
+          res.status(200).json({
+            OK: `Postarea și imaginile asociate au fost trimise cu succes.`,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: `S-a produs o eroare la inserarea imaginilor: ${err}`,
+          });
+        });
     }
-  );
+  });
 });
 
 module.exports = router;
--- frontend/src/components/pages/Profile/newPost/newPostPopUp.js
+++ frontend/src/components/pages/Profile/newPost/newPostPopUp.js
@@ -1,7 +1,73 @@
-import { Button, FormLabel } from "@mui/material";
+import {
+  Button,
+  FormLabel,
+  TextField,
+  TextareaAutosize as BaseTextareaAutosize,
+} from "@mui/material";
+import { styled } from "@mui/system";
 import "./popUpStyle.css";
 
 const NewPost = ({ onClose }) => {
+  const blue = {
+    100: "#DAECFF",
+    200: "#b6daff",
+    400: "#3399FF",
+    500: "#007FFF",
+    600: "#0072E5",
+    900: "#003A75",
+  };
+
+  const grey = {
+    50: "#F3F6F9",
+    100: "#E5EAF2",
+    200: "#DAE2ED",
+    300: "#C7D0DD",
+    400: "#B0B8C4",
+    500: "#9DA8B7",
+    600: "#6B7A90",
+    700: "#434D5B",
+    800: "#303740",
+    900: "#1C2025",
+  };
+  const Textarea = styled(BaseTextareaAutosize)(
+    ({ theme }) => `
+    box-sizing: border-box;
+    width: 320px; 
+    height: 100px;
+    resize: none;
+    overflow: auto;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 8px 12px;
+    border-radius: 8px;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${
+      theme.palette.mode === "dark" ? grey[900] : grey[50]
+    };
+    @media (max-width: 600px) {
+      width: 100%; // Lățimea se ajustează la dimensiunea containerului
+      height: 80px; // Înălțime mai mică pentru spațiu limitat
+    }
+    &:hover {
+      border-color: ${blue[400]};
+    }
+  
+    &:focus {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${
+        theme.palette.mode === "dark" ? blue[600] : blue[200]
+      };
+    }
+  
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `
+  );
   return (
     <div className="popup-container">
       <div className="popup">
@@ -13,8 +79,17 @@ const NewPost = ({ onClose }) => {
             X
           </Button>
         </div>
-        <h2>Popup Content</h2>
-        <p>This is the content of the popup.</p>
+        <div className="popup-Body">
+          <TextField id="standard-basic" label="Title" variant="outlined" />
+          <Textarea
+            style={{ marginTop: "10px" }}
+            fixated
+            aria-label="Demo input"
+            minRows={3}
+            variant="outlined"
+            placeholder="What's in your mind"
+          />
+        </div>
       </div>
     </div>
   );
